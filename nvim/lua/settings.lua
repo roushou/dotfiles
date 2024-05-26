d84d323271f9d72ac74bf88c2844ab7143415eb5
roushou <roushou9@gmail.com>
2024-05-26 13:45:53 +0700

    chore(nvim): update settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,12 +2,14 @@ vim.g.mapleader = " "
 
 local opt = vim.opt
 
+opt.hidden = true
 opt.number = true
 opt.relativenumber = true
 opt.tabstop = 2
-opt.tabstop = 2
 opt.shiftwidth = 2
+opt.smartindent = true
 opt.scrolloff = 8
+opt.signcolumn = 'yes' -- Always show the sign column
 opt.splitright = true
 opt.splitbelow = true
 -- preview substitutions live
