4f4246a184eff127c2bf9177af55e37949fc05f7
roushou <roushou9@gmail.com>
2024-05-30 16:03:24 +0700

    chore: move wezterm config into its folder

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,18 +2,18 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'JetBrains Mono'
-config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font("JetBrains Mono")
+config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
-config.default_cursor_style = 'SteadyBlock'
+config.default_cursor_style = "SteadyBlock"
 config.warn_about_missing_glyphs = false
 
 config.keys = {
 	{
-		key = 'P',
-		mods = 'CTRL',
+		key = "P",
+		mods = "CTRL",
 		action = wezterm.action.DisableDefaultAssignment,
-	}
+	},
 }
 
 return config
