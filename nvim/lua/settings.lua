0e755806f6445db9e7bd5b446f742ebd835e5b46
roushou <roushou9@gmail.com>
2024-07-10 01:20:20 +0700

    feat(nvim): add format on save for Go

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -22,6 +22,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
 		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "go",
+	callback = function()
 		require("go.format").goimports()
 	end,
 })
