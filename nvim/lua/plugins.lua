38cf52c76dfe8684d20a77f8ba9a041e8ea36805
roushou <roushou9@gmail.com>
2025-03-13 18:15:01 +0700

    chore(nvim): update plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -14,10 +14,13 @@ require("lazy").setup({
 		"echasnovski/mini.nvim",
 		version = "*",
 		config = function()
+			require("mini.ai").setup()
 			require("mini.comment").setup()
 			require("mini.pairs").setup()
 			require("mini.notify").setup()
+			require("mini.files").setup()
 			require("mini.pick").setup()
+			require("mini.jump2d").setup()
 			require("mini.bufremove").setup()
 			require("mini.starter").setup()
 			require("mini.statusline").setup()
@@ -51,13 +54,17 @@ require("lazy").setup({
 	},
 	{
 		"ray-x/go.nvim",
-		dependencies = {},
+		dependencies = {
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
 		config = function()
 			require("go").setup()
 		end,
 		event = { "CmdlineEnter" },
 		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+		build = ':lua require("go.install").update_all_sync()',
 	},
 	{ "folke/neodev.nvim", opts = {} },
 	{
@@ -145,7 +152,10 @@ require("lazy").setup({
 			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
 	},
-	{ "lewis6991/gitsigns.nvim", opts = {} },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {},
+	},
 	{
 		"windwp/nvim-ts-autotag",
 		event = "BufReadPre",
