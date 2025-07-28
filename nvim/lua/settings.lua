e236a091043059dd117b70bc5099f67728bb53b6
roushou <roushou9@gmail.com>
2025-07-29 03:50:20 +0900

    chore(nvim): set wgsl indent size

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -49,6 +49,14 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
+	pattern = "wgsl",
+	callback = function()
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.tabstop = 4
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
 	pattern = "go",
 	callback = function()
 		vim.opt_local.shiftwidth = 4
